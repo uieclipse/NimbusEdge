0b0977c1dfb52330b2ffac5cdc7f7115aadef43e
sanjay <86116863+uieclipse@users.noreply.github.com>
2026-06-19 20:21:32 +0000

    Refactor routing and deployment configuration for React app

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/NimbusEdge">
+    <BrowserRouter >
       <App />
     </BrowserRouter>
   </StrictMode>,
